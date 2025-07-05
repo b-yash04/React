6d1a946547ff90300014c376a764bded5d116bce
yash bhatt <bhatty048@gmail.com>
2025-07-05 18:09:54 +0530

    Travel Journal project with react

---
+++ Travel Journal/src/App.jsx
@@ -0,0 +1,10 @@
+import Nav from "./components/Nav.jsx"
+
+function App(){
+  return(
+    <>
+      <Nav/>
+    </>
+  )
+}
+export default App;
---
+++ Travel Journal/src/components/Nav.jsx
@@ -0,0 +1,9 @@
+export default function Nav(){
+    return(
+        <nav className="navbar">
+            <img src="globe.png" alt="globe" />
+            <h1>my travel journal.</h1>
+        </nav>
+    )
+}
+
